99d17551eb1bd7af2cda94345cb5e44c8a87ca2d
Josue Castillo <30334352+josuedavi@users.noreply.github.com>
2021-05-29 20:47:28 -0400

    alarm clock finished

--- reactproj#5/alarm_clock/src/App.js
+++ reactproj#5/alarm_clock/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Clock from './Clock';
 
-import AppBar from './Form';
+import DropDownData from './Form';
 import './App.css';
 
 
@@ -23,7 +23,7 @@ class App extends React.Component {
         <div style={clockStyle}>
         <Clock />
       
-         <AppBar />
+         <DropDownData />
        </div>  
        
       </div>
